c0a5a5c6a18c2b56f86fc5135e325f2af7c3784a
Laura Rose Mary Hansen <laurarosemaryhansen@gmail.com>
2022-02-06 16:29:49 +0000

    updates on about me

--- java.js
+++ java.js
@@ -7,3 +7,15 @@ function mobileMenu(){
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
 }
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("banner").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
